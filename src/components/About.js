86d7a90beeda4b5d4a9b1d7454e93cc3da895981
invisibleminer12345 <gaoruiqi09@gmail.com>
2025-05-15 06:49:25 -0600

    Improved GUI of Homepage and Reservations

--- src/components/About.js
+++ src/components/About.js
@@ -27,17 +27,17 @@ const About = () => {
                 <h1 className="head">Who Are We?</h1>
             </div>
             <div className="info">
-                <h4 className="desc">
+                <p className="desc">
                 The Teen Cafe is just one of the programs run by the Calgary Tsung Tsin Benevolent Association, a charity founded on the basis of connecting cultures around the city. Subsequently, membership to the Calgary Teen Cafe also includes membership to all other programs run by the Calgary Tsuin Tsin Benevolent Association. This program, also called the youth connection program, is completely youth-ran. From volunteering to leadership, the Cafe provides opportunities for youth to make an impact improving diversity in the next generation.
-                </h4>
+                </p>
             </div>
             <div className="offer">
                 <h1 className="head">Our Mission</h1>
             </div>
             <div className="info">
-                <h4 className="desc">
+                <p className="desc">
                 Calgary Teen Cafe program was founded on the basis of providing unbiased opportunity to everyone, regardless of their background. We firmly believe that this can be accomplished through modern technology. Through the endless opportunities and experiences it creates, technology allows users to bond through shared experiences in a low-pressure environment. Unfortunately, not everyone has access to this luxury. Thus, the Cafe’s mission is to provide a public space for all youth to easily network with others.
-</h4>
+</p>
             </div>
             <svg width="0" height="0">
                 <linearGradient id="blue-gradient" x1="100%" y1="100%" x2="0%" y2="0%">
